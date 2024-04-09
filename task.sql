752f463a0886c829e235154bbff59708a72b4a07
Iryna Lyakhova <ippk0629@gmail.com>
2024-04-09 18:17:10 +0300

    Add index

--- task.sql
+++ task.sql
@@ -6,3 +6,5 @@ CREATE INDEX idx_Customers_Email ON Customers (Email);
 CREATE INDEX idx_Products_Name ON Products (Name);
 
 CREATE INDEX idx_Orders_Date ON Orders (Date);
+
+CREATE INDEX idx_OrderItems_ProductID ON OrderItems (ProductID);
